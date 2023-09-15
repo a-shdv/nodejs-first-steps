c93639b99514bea12192a5ebe736ee777d24aa61
a-shdv <shadaev2001@icloud.com>
2023-09-16 01:33:23 +0400

    Express js start

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const http = require('http')
+import express from 'express'
+import path from 'path'
+
+const app = express()
+
+// to use templates engine use the following:
+app.set('view engine', 'ejs')
 
 const PORT = 3000
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method) // url '/', method 'GET'
+const createPath = (page) => path.resolve(`./templates/${page}`)
 
-    res.setHeader('Content-Type', 'application/json')
-
-    const data = JSON.stringify([
-        { id: 1, name: 'John', surname: 'Doe' },
-        { id: 2, name: 'Jane', surname: 'Doe'},
-        {id: 3, name: 'test', surname: 'test', hobbies: ['jogging', 'music', 'programming']}
-    ])
-
-    res.write(data)
-
-    res.end()
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`The server has been started on port ${PORT}`)
 })
 
-server.listen(PORT, 'localhost', (error) => {
-    error ? console.log(error) : console.log(`The server has been started on port ${PORT}`)
+app.get('/', (req, res) => {
+    res.render(createPath('index.ejs'))
+})
+
+app.get('/about', (req, res) => {
+    res.render(createPath('about.ejs'))
+})
+
+// use() method to create a middleware
+app.use((req, res) => {
+    res
+        .status(404)
+        .render (createPath('error.ejs'))
 })
