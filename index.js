9f0d6f62e07a4a140b10174324c9c16558dc9795
a-shdv <shadaev2001@icloud.com>
2023-09-15 18:56:58 +0400

    File deletion

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const os = require('os')
+const fs = require('fs')
 
-console.log(os.platform(), os.release())
+let newData = ''
+
+// READ
+// async function
+fs.readFile('./text.txt', (error, data) => {
+    console.log(data.toString())
+
+    // DIRECTORY CREATION
+    // sync function
+    // Синхронные методы для того, чтобы задать порядок выполнения методов
+    fs.mkdirSync('./files', () => {})
+
+    // WRITE
+    // sync function
+    fs.writeFileSync('./files/another-text.txt', data, (error) => {
+        error ? console.log(error) : null
+    })
+})
+
+setTimeout(() => {
+    // FILE DELETION
+    fs.unlink('./files/another-text.txt', () => {})
+    console.log('File has been succesfully deleted!')
+}, 5000)
+
+setTimeout(() => {
+    // DIRECTORY DELETION
+    fs.rmdir('./files', () => {})
+    console.log('Directory has been succesfully deleted!')
+}, 10000)
--- modules.js
+++
@@ -1,4 +0,0 @@
-const username = "a-shdv"
-const sayHello = (test) => `Hello, my name is ${test}`
-// module.exports = username
-module.exports = {username, sayHello}
