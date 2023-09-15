f2c2befb894034ca71437dcd08abc97cc34fe2df
a-shdv <shadaev2001@icloud.com>
2023-09-15 18:21:39 +0400

    Modules

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// GLOBAL OBJECTS
-console.log(__dirname) // current directory absolute path
-console.log(__filename) // current file absolute path
+const {username: user, sayHello: say} = require('./modules')
 
-console.log(process) // useful variables
-console.log(process.env) // sample
-console.log(process.argv) // passing data through terminal
-
-const url = new URL('https://localhost:8080/path/to#name?age=21')
-console.log(url.hostname)
-console.log(url.href)
-console.log(url.pathname)
-console.log(url.hash)
+console.log(say(user))
---
+++ modules.js
@@ -0,0 +1,4 @@
+const username = "a-shdv"
+const sayHello = (test) => `Hello, my name is ${test}`
+// module.exports = username
+module.exports = {username, sayHello}
